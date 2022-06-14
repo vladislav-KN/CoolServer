9cac304c76d40ac0b2bd8fac992a4e74326c4430
vladisloveK <vnkolevatov@gmail.com>
2022-06-15 02:31:10 +0500

    Prover

--- CoolServer/Controllers/MessageController.cs
+++ CoolServer/Controllers/MessageController.cs
@@ -27,12 +27,12 @@ namespace CoolServer.Controllers
         /// <response code="200">Сообщения получены</response>
         /// <response code="400">Ошибки возникшие при попытке получить сообщения</response>
         /// <response code="500">Сервер не отвечает</response>
-        [HttpGet("{portion}/{offset}")]
+        [HttpGet("{chatId}/{portion}/{offset}")]
         [ProducesResponseType(typeof(Message), 200)]
         [ProducesResponseType(204)]
         [ProducesResponseType(typeof(ProblemDetails), 400)]
         [ProducesResponseType(500)]
-        public async Task<ActionResult<IEnumerable<Message>>> GetAsync(Chat chat, int portion, int offset)
+        public async Task<ActionResult<IEnumerable<Message>>> GetAsync(Guid chatId, int portion, int offset)
         {
             StringValues token;
             if (!Request.Headers.TryGetValue("Token", out token))
@@ -48,7 +48,7 @@ namespace CoolServer.Controllers
                     StatusCode = 400
                 };
             }
-            var result = await RequestApi<MessagesPortionDetails, Guid>.Get($"Messages?chatId={chat.Id}&offset={offset}&portion={portion}", token.ToString());
+            var result = await RequestApi<MessagesPortionDetails, Guid>.Get($"Messages?chatId={chatId}&offset={offset}&portion={portion}", token.ToString());
             if (result.Item2 == System.Net.HttpStatusCode.OK)
             {
                 List<Message> messages = new List<Message>();
@@ -62,6 +62,7 @@ namespace CoolServer.Controllers
                     {
                         var user = await RequestApi<UserDetails, Guid>.Get($"Users/{message.SenderId}", token.ToString());
                         if (user.Item2 == System.Net.HttpStatusCode.OK)
+                        {
                             messages.Add(new Message()
                             {
                                 Id = message.Id,
@@ -71,6 +72,7 @@ namespace CoolServer.Controllers
                                 Text = message.Text
                             }
                         );
+                        }
 
                     }
                     return messages;
@@ -81,6 +83,55 @@ namespace CoolServer.Controllers
                 }
                 
             }
+            else
+            {
+                return new ObjectResult(result.Item2)
+                {
+                    StatusCode = (int)result.Item2
+                };
+            }
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Message), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<Message>> GetMessageAsync(Guid id)
+        {
+            StringValues token;
+            if (!Request.Headers.TryGetValue("Token", out token))
+            {
+                ProblemDetails problem = new ProblemDetails()
+                {
+                    Detail = "Server didn't receive Token ",
+                    Status = 400,
+                    Title = "Access denied"
+                };
+                return new ObjectResult(problem)
+                {
+                    StatusCode = 400
+                };
+            }
+            var result = await RequestApi<MessageDetails, Guid>.Get($"Messages?id={id}", token.ToString());
+            if (result.Item2 == System.Net.HttpStatusCode.OK)
+            {
+                var user = await RequestApi<UserDetails, Guid>.Get($"Users/{ result.Item1.SenderId}", token.ToString());
+                if (user.Item2 == System.Net.HttpStatusCode.OK)
+                {
+
+                    Message message = new Message()
+                    {
+                        Id = result.Item1.Id,
+                        Attachments = result.Item1.Attachments,
+                        IsViewed = result.Item1.IsViewed,
+                        Sender = new User() { Id = user.Item1.Id, Login = user.Item1.Login },
+                        Text = result.Item1.Text,
+                    };
+                    return message;
+                }
+                return StatusCode(204);
+            }
+            
             else
             {
                 return new ObjectResult(result.Item2)
